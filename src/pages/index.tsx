6050b1a0d08e9268d51b3e11c8b501ca95dfd4e3
byeongminn <sekn1997@naver.com>
2024-06-23 20:08:35 +0900

    feat: 선택한 식당 UI 구현 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
-import { Map } from '@/components';
+import { Map, Markers, StoreBox } from '@/components';
+
+import * as stores from '@/data/store_data.json';
+import { useState } from 'react';
 
 export default function Home() {
-  return <Map />;
+  const [map, setMap] = useState(null);
+  const [currentStore, setCurrentStore] = useState(null);
+  const storeDatas = stores['DATA'];
+
+  return (
+    <>
+      <Map setMap={setMap} />
+      <Markers
+        map={map}
+        storeDatas={storeDatas}
+        setCurrentStore={setCurrentStore}
+      />
+      <StoreBox store={currentStore} setStore={setCurrentStore} />
+    </>
+  );
 }
