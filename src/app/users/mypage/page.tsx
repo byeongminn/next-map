d8592860d2145f990750ecc3ec0d9d2920134365
byeongminn <sekn1997@naver.com>
2024-06-28 18:19:18 +0900

    fix: Pagination이 제대로 작동하지 않는 버그 수정

--- src/app/users/mypage/page.tsx
+++ src/app/users/mypage/page.tsx
@@ -7,9 +7,13 @@ import axios from 'axios';
 import { signOut, useSession } from 'next-auth/react';
 import { useQuery } from 'react-query';
 
-export default function MyPage({ params }: { params: { page: string } }) {
+export default function MyPage({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   const { data: session } = useSession();
-  const page = params?.page || '1';
+  const page = searchParams?.page || '1';
 
   const fetchComments = async () => {
     const { data } = await axios(
@@ -94,7 +98,7 @@ export default function MyPage({ params }: { params: { page: string } }) {
       <Pagination
         total={comments?.totalPage}
         page={page}
-        pathname="/user/mypage"
+        pathname="/users/mypage"
       />
     </div>
   );
